df2da74e59a6306f3cf13c3a87ce8a2b8f5a09e8
Sarah Pai <pai.sarah89@gmail.com>
2019-02-22 18:39:15 -0500

    Updates

---
+++ precache-manifest.84772ba8efbea3fc54581196d49490e4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "942e704b332b6457aa07",
+    "url": "/static/css/main.27687e10.chunk.css"
+  },
+  {
+    "revision": "942e704b332b6457aa07",
+    "url": "/static/js/main.942e704b.chunk.js"
+  },
+  {
+    "revision": "e186b8187c343fdf7533",
+    "url": "/static/js/1.e186b818.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dd12ef0b67e3b0e50c18d5d40ab44402",
+    "url": "/static/media/youtube.dd12ef0b.png"
+  },
+  {
+    "revision": "8b7bbc73d091bf535770a13687eaf584",
+    "url": "/static/media/profilepic.8b7bbc73.jpg"
+  },
+  {
+    "revision": "b1eaeca622955a0b318f9493afc63ce6",
+    "url": "/static/media/reactbooth.b1eaeca6.png"
+  },
+  {
+    "revision": "3fcffc40e7106705ea71c8e29d3c7b68",
+    "url": "/static/media/yelpntable.3fcffc40.png"
+  },
+  {
+    "revision": "3a80950025b893c2915dc23ed7a697c4",
+    "url": "/index.html"
+  }
+];
