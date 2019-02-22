a515fcd4e239ab5e6dc8b87cabc3936905d3432a
Sarah Pai <pai.sarah89@gmail.com>
2019-02-22 13:09:34 -0500

    landing page designed

--- src/Component/Homepage.js
+++ src/Component/Homepage.js
@@ -2,20 +2,20 @@ import React from 'react'
 import About from './About'
 import computer from '../assets/images/computer-placeholder.jpg'
 import drawing from '../assets/images/drawing-placeholder.jpg'
-import '../assets/css/app.css'
+import '../assets/css/homepage.css'
 class Homepage extends React.Component {
 	render() {
 		return (
 			<>
-				<div className="row">
-					<div className="column">
+			
+				<div className="pic">
+					<div className="pic">
 						<img width={700} height={700} src={computer} alt="computer"></img>
 					</div>
-					<div className="column">
+					<div className="pic">
 						<img width={700} height={700} src={drawing} alt="drawing"></img>
 					</div>
 				</div>
-			<hr></hr>
 				
 			</>
 		)
--- src/Component/menubar.js
+++ src/Component/menubar.js
@@ -2,38 +2,62 @@ import React from 'react';
 import '../assets/css/menubar.css'
 import computer from '../assets/images/computer-placeholder.jpg'
 import drawing from '../assets/images/drawing-placeholder.jpg'
-import '../assets/css/app.css'
 
 class Menubar extends React.Component {
 
 	render() {
 		return (
 			<>
-				<div className="wrapper">
-					<div className="column">
-							<img width={700} height={700} src={drawing} alt="drawing"></img>
-					</div>
-					<div className="column">
-							<img width={700} height={700} src={computer} alt="computer"></img>
-					</div>
-					<div className="line">
-						<div className="wordwrapper">
-							<div className="word">
-							ABOUT
-							</div> 
-						</div>
-					</div>
-				</div>
-				<div className="wrapper">
+				<section className="row">
+					<div className="column-one">
+						<a href="www.linkedin.com/in/sarahpai">
+							<i className="fab fa-linkedin-in fa-2x"></i>
+						</a>
+						<a href="www.medium.com/paisarah89">
+							<i class="fab fa-medium-m fa-2x"></i>
+						</a>
+						<a href="www.github.com/sarahpai">
+					    	<i class="fab fa-github fa-2x"></i>
+						</a>
 					
-					<div className="line">
+						{/* <div className="line">
 						<div className="wordwrapper">
-							<div className="word" id="work">
-								WORK
+							<a className="word">
+							ABOUT
+							</a> 
 							</div>
 						</div>
+					<div className="wrapper">
+					  <div className="line">
+						<div className="wordwrapper">
+							<a className="word" id="work">
+								WORK
+							</a>
+						</div>
+					  </div>
+					</div> */}
+						<article>
+							<span>HELLO!</span>
+							<p>scroll to see my work</p>
+						</article>
+					</div>
+					<div className="column-two">
+						<article>
+					
+						</article>
+					</div>
+				</section>
+				
+				<div className="wrapper">
+					<div className="line">
+						<div className="wordwrapper">
+							<a className="word">
+							ABOUT
+							</a> 
+						</div>
 					</div>
 				</div>
+			
 			
 			
 				</>
